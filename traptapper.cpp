454719dd4737768f4f38fb0252f56cab3d82e794
Drew Stevens <ahstevens@gmail.com>
2015-09-28 21:14:00 -0400

    add binary release and tidy up code a bit

--- traptapper.cpp
+++ traptapper.cpp
@@ -1,3 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////
+// TRAPTAPPER v.1
+// by Drew Stevens (ahstevens@gmail.com)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+///////////////////////////////////////////////////////////////////////////////
+
+
 #include <iostream>
 #include <tesseract/baseapi.h>
 #include <leptonica/allheaders.h>
@@ -6,9 +22,8 @@
 #include <boost/algorithm/string.hpp>
 #include <cctype>
 #include <vector>
+#include <sys/stat.h>
 
-#define M1_TOP 2345
-#define M1_HT 100
 
 std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
 	std::stringstream ss(s);
@@ -25,6 +40,11 @@ std::vector<std::string> split(const std::string &s, char delim) {
 	return elems;
 }
 
+bool file_exists(const std::string& name) {
+	struct stat buffer;
+	return (stat(name.c_str(), &buffer) == 0);
+}
+
 int main( int argc, char* argv[] )
 {
 	if ( argc != 4 ){
@@ -32,13 +52,7 @@ int main( int argc, char* argv[] )
 		return 0;
 	}
 
-	std::ofstream outFile(argv[1]);
-	if (!outFile.is_open())
-	{
-		std::cerr << "ERROR: Could not open output file " << argv[1] << std::endl;
-		exit(1);
-	}
-
+	/**************************** TESSERACT INIT *****************************/
 	tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
 	// Initialize tesseract-ocr with English, without specifying tessdata path
 	if ( api->Init( NULL, "eng" ) ) {
@@ -46,10 +60,15 @@ int main( int argc, char* argv[] )
 		exit( 1 );
 	}
 	
-	//std::string pressure, temperature_raw, temperature, date, time, cameraType;
 	std::string line;
 
 	/**************************** CONFIG FILE ********************************/
+	if ( !file_exists(std::string( argv[2] ) ) )
+	{ 
+		std::cerr << "ERROR: Could not find config file: " << std::string(argv[2]) << std::endl;
+		exit( 1 );
+	}
+
 	std::ifstream configFile( argv[2] );
 	std::vector< std::vector< std::string > > config;
 
@@ -65,7 +84,14 @@ int main( int argc, char* argv[] )
 		config.push_back( split( line, ' ' ) );
 	}
 
-	/**************************** OUTPUT HEADERS *****************************/
+	/**************************** OUTPUT FILE ********************************/
+	std::ofstream outFile(argv[1]);
+	if (!outFile.is_open())
+	{
+		std::cerr << "ERROR: Could not open output file " << argv[1] << std::endl;
+		exit(1);
+	}
+
 	// begin header with filepaths
 	outFile << "filepath,cameraType";
 
@@ -86,6 +112,12 @@ int main( int argc, char* argv[] )
 	int imageCount = 0;
 	while (std::getline(imagelist, line))
 	{
+		if (!file_exists(line))
+		{
+			std::cerr << "ERROR: Could not find image file: " << line << std::endl;
+			continue;
+		}
+
 		Pix *image = pixRead(line.c_str());
 		api->SetImage(image);
 
